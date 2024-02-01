ed0355b113fe95a2ea6fa06f07aac7158c029b32
saarryp <s.rypkema@casema.nl>
2024-02-01 16:33:11 +0100

    opzet eindproject IntellJ start

--- src/main/java/nl/bitsentools/eineindprojectbackendmetabo/EineindprojectBackEndMetaboApplication.java
+++ src/main/java/nl/bitsentools/eineindprojectbackendmetabo/EineindprojectBackEndMetaboApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EineindprojectBackEndMetaboApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EineindprojectBackEndMetaboApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EineindprojectBackEndMetaboApplication.class, args);
+    }
 
 }
