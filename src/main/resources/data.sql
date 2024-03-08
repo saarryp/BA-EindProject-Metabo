0b313b6ec2ae6d3c1bb3a14a4b788680c75f9795
saarryp <s.rypkema@casema.nl>
2024-03-08 12:09:27 +0100

    Changed the name stap5 to stap5-relaties, but doesnt rename in remote. Try to run hardcoded order etc in data-sql but doesn't run like it should.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,27 @@
-INSERT INTO products(id, brand_name,product_number, product_name, price, type_of_machine, product_warranty, warranty_in_months)
-values (1,'test', 1,'testName',300, 'SCHUURMACHINE',false, 0 );
+-------------------------------PRODUCTS----------------------------------------------
 
+INSERT INTO products(brand_name, product_name, price,
+
+                     type_of_machine, product_warranty, warranty_in_months)
+values ('test', 'testName',300, 'SCHUURMACHINE',true, 5 );
+
+-------------------------------ORDERS--------------------------------------
+
+INSERT INTO orders(user_id, product_number, user_email, user_details, order_number, price, quantity, total_price_order)
+VALUES (1, 1, 'saskia@hotmail.com', 'saskia noord prinsenstraat 12 2415 XG Edam', 1, 300, 2, 600);
+
+
+--------------------------------WARRANTIES------------------------------------------------
+
+
+
+
+
+---------------------------------INVOICE----------------------------------------------------
+
+
+
+
+
+
+-------------------------------USERS---------------------------------------------------
