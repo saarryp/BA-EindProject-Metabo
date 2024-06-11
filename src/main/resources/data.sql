19e2d67de8277b1db71169d0f7defecdc242555d
saarryp <s.rypkema@casema.nl>
2024-06-11 16:16:53 +0200

    addition of requestmatchers for userController.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,18 +4,33 @@ INSERT INTO products(id, brand_name, product_name, product_number, price, type_o
 values (101,'test', 'testName',1, 300, 'SCHUURMACHINE',true, 5),
        (102, 'test2', 'testname2', 2,  60, 'ZAAGMACHINE', false, 0);
 
+-- Voeg de kolom "product_id" toe aan de "orders" tabel
+ALTER TABLE orders
+    ADD COLUMN product_id INTEGER;
+
+
 -------------------------------ORDERS--------------------------------------
 
 INSERT INTO orders(id, user_id, product_number, user_email, user_details, order_number, price, quantity, total_price_order)
 VALUES (101,1, 1, 'saskia@hotmail.com', 'saskia noord prinsenstraat 12 2415 XG Edam', 1, 300, 2, 600),
        (102, 2, 2, 'bertus@gmail.com', 'berts beukerhuis bedrijventerrein zichters hollandse rading', 2, 20, 5, 100);
 
+-------------------------kolom toevoegen van product en orders-------------
 
---------------------------------WARRANTIES------------------------------------------------
 
-INSERT INTO warranties(id, product_number, warranty_start, warranty_ends)
-VALUES (101, 1, '2024-05-17', '2025-07-17'),
-       (102, 2, '2023-12-24', '2026-01-01');
+UPDATE orders
+SET product_id = (
+    CASE
+        WHEN order_number = 1 THEN 101
+        WHEN order_number = 2 THEN 102
+        -- Voeg hier meer WHEN-clausules toe voor andere order numbers
+        ELSE NULL
+        END
+    )
+WHERE order_number IN (1, 2);
+
+
+
 
 ----------------------------------STOCKS--------------------------------------------------
 INSERT INTO stocks(id,brand_name, product_name, product_number, type_of_machine,
@@ -41,6 +56,30 @@ INSERT INTO invoices ( id,
          );
 
 
+--------------------------------WARRANTIES------------------------------------------------
+
+INSERT INTO warranties(id, product_number, warranty_start, warranty_ends)
+VALUES (101, 1, '2024-05-17', '2025-07-17'),
+       (102, 2, '2023-12-24', '2026-01-01');
+
+-- -------------------------------INVOICES & WARRANTIES----------------------------------------
+-- -- Voeg de kolom "warranty_id" toe aan de "invoices" tabel
+-- ALTER TABLE invoices
+--     ADD COLUMN warranty_id INTEGER;
+--
+--
+-- -- Factuur 101 koppelen aan garantie 101
+-- UPDATE invoices
+-- SET warranty_id = 101
+-- WHERE id = 101;
+--
+-- -- Factuur 102 koppelen aan garantie 102
+-- UPDATE invoices
+-- SET warranty_id = 102
+-- WHERE id = 102;
+
+
+
 -------------------------------USERS-------------------------------------------------
 
 -- password password
@@ -57,3 +96,6 @@ VALUES (1, 'ROLE_CLIENT');
 
 INSERT INTO authorities (id, authority)
 VALUES (2, 'ROLE_ADMIN');
+
+
+--KOPPELINGEN MAKEN TUSSEN INVOICES & WARRANTY (ONETOONE) EN ORDER & PRODUCT (MANYTOMANY) PRODUCT & IMAGEDATA (ONETOMANY)
